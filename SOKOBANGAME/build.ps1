7a9c6d893fbf2d64efb99e779e8e7ec56e27bed4
JoanneAviccien <inijohan29@gmail.com>
2025-06-01 15:12:54 +0700

    Membuat configurasi compiler dengan cmake dan make

---
+++ SOKOBANGAME/build.ps1
@@ -0,0 +1,5 @@
+mkdir BUILD
+cd BUILD
+echo "Melakukan Compiling Untuk Game Sokoban.."
+cmake -S .. -G "MinGW Makefiles"
+make
--- SOKOBANGAME/main.c
+++ SOKOBANGAME/main.c
@@ -2,41 +2,40 @@
 
 int main() {
 
-    setlocale(LC_ALL, "");
+  setlocale(LC_ALL, "");
+  initscr();
 
-    initscr();
-    
-    // Inisialisasi warna
-    start_color();
-    init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);
-    init_pair(3, COLOR_BLACK, COLOR_BLACK);
-    init_pair(4, COLOR_YELLOW, COLOR_BLACK);
+  // Inisialisasi warna
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_GREEN, COLOR_BLACK);
+  init_pair(3, COLOR_BLACK, COLOR_BLACK);
+  init_pair(4, COLOR_YELLOW, COLOR_BLACK);
 
-    cbreak();
-    noecho();
-    curs_set(FALSE);
-    keypad(stdscr, TRUE);
-    timeout(0);
+  cbreak();
+  noecho();
+  curs_set(FALSE);
+  keypad(stdscr, TRUE);
+  timeout(0);
 
-    // Tampilkan lobby screen dan ambil pilihan user
-    int menu_choice = show_lobby_screen();
-    RoomLayout room;
+  // Tampilkan lobby screen dan ambil pilihan user
+  int menu_choice = show_lobby_screen();
+  RoomLayout room;
 
-    while (menu_choice != 4) {
-        // Handle pilihan menu
-        switch (menu_choice) {
-            case 0: // Play Game
-                start_level(&room, Level_1c3_map, Level_1c3);
-                break;
-            case 1: // History
-                // Implementasi fitur history
-                break;
-            case 4: // Quit Game
-                break;
-        }
+  while (menu_choice != 4) {
+    // Handle pilihan menu
+    switch (menu_choice) {
+    case 0: // Play Game
+      start_level(&room, Level_1c3_map, Level_1c3);
+      break;
+    case 1: // History
+      // Implementasi fitur history
+      break;
+    case 4: // Quit Game
+      break;
     }
+  }
 
-    endwin();
-    return 0;
+  endwin();
+  return 0;
 }
