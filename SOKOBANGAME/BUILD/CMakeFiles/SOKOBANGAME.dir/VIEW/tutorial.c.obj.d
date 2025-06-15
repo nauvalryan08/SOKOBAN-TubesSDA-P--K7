a83be83d3077bee230464739ae9371a152b0f88e
JoanneAviccien <inijohan29@gmail.com>
2025-06-15 15:41:52 +0700

    Perbaikan Cmakelists serta perubahan sound effect

--- SOKOBANGAME/BUILD/CMakeFiles/SOKOBANGAME.dir/VIEW/tutorial.c.obj.d
+++ SOKOBANGAME/BUILD/CMakeFiles/SOKOBANGAME.dir/VIEW/tutorial.c.obj.d
@@ -1,25 +1,25 @@
 CMakeFiles/SOKOBANGAME.dir/VIEW/tutorial.c.obj: \
- C:\Teknik\ Infomatika\StrukturDataAlgoritma\UAS\SOKOBANGAME\VIEW\tutorial.c \
- C:\Teknik\ Infomatika\StrukturDataAlgoritma\UAS\SOKOBANGAME\VIEW\tutorial.h \
- C:/TEKNIK~1/STRUKT~1/UAS/SOKOBA~1/UTILS/include/curses.h \
- C:/MinGW/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdarg.h \
- C:/MinGW/x86_64-w64-mingw32/include/stdarg.h \
- C:/MinGW/x86_64-w64-mingw32/include/_mingw_stdarg.h \
- C:/MinGW/x86_64-w64-mingw32/include/vadefs.h \
- C:/MinGW/x86_64-w64-mingw32/include/_mingw.h \
- C:/MinGW/x86_64-w64-mingw32/include/_mingw_mac.h \
- C:/MinGW/x86_64-w64-mingw32/include/_mingw_secapi.h \
- C:/MinGW/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
- C:/MinGW/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stddef.h \
- C:/MinGW/x86_64-w64-mingw32/include/stddef.h \
- C:/MinGW/x86_64-w64-mingw32/include/crtdefs.h \
- C:/MinGW/x86_64-w64-mingw32/include/corecrt.h \
- C:/MinGW/x86_64-w64-mingw32/include/stdio.h \
- C:/MinGW/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
- C:/MinGW/x86_64-w64-mingw32/include/_mingw_off_t.h \
- C:/MinGW/x86_64-w64-mingw32/include/swprintf.inl \
- C:/MinGW/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
- C:/MinGW/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdbool.h \
- C:/MinGW/x86_64-w64-mingw32/include/string.h \
- C:/MinGW/x86_64-w64-mingw32/include/sec_api/string_s.h \
- C:/MinGW/x86_64-w64-mingw32/include/locale.h
+ C:\Users\Administrator\Documents\Tugas\Tambal\ Tubes\ Ban\SOKOBAN-TubesSDA-P--K7\SOKOBANGAME\VIEW\tutorial.c \
+ C:\Users\Administrator\Documents\Tugas\Tambal\ Tubes\ Ban\SOKOBAN-TubesSDA-P--K7\SOKOBANGAME\VIEW\tutorial.h \
+ C:/Users/ADMINI~1/DOCUME~1/Tugas/TAMBAL~1/SOKOBA~1/SOKOBA~1/UTILS/include/curses.h \
+ C:/ProgramData/mingw64/mingw64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdarg.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/stdarg.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/ProgramData/mingw64/mingw64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stddef.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/ProgramData/mingw64/mingw64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdbool.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/string.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/locale.h
